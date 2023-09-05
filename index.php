908ac2e5641ec92357ac282006eb47e03a0293f0
MasterCard31 <carlos.mendoza.s@tecsup.edu.pe>
2023-09-05 09:12:46 -0500

    Algunos cambios

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
 
 <body class="home">
 	<!-- Fixed navbar -->
-	<div class="navbar navbar-inverse navbar-fixed-top headroom" >
+	<div class="navbar navbar-inverse bg-rojo navbar-fixed-top headroom" >
 		<div class="container">
 			<div class="navbar-header">
 				<!-- Button for smallest screens -->
@@ -36,17 +36,17 @@
 			</div>
 			<div class="navbar-collapse collapse">
 				<ul class="nav navbar-nav pull-right">
-					<li class="active"><a href="#">Home</a></li>
-					<li><a href="about.php">About</a></li>
+					<li class="active"><a href="#">Inicio</a></li>
+					<li><a href="about.php">Sobre Nosotros</a></li>
 					<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">More Pages <b class="caret"></b></a>
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Más páginas <b class="caret"></b></a>
 						<ul class="dropdown-menu">
-							<li><a href="sidebar-left.php">Left Sidebar</a></li>
-							<li class="active"><a href="sidebar-right.php">Right Sidebar</a></li>
+							<li><a href="sidebar-left.php">Barra lateral izquierda</a></li>
+							<li class="active"><a href="sidebar-right.php">Barra lateral derecha</a></li>
 						</ul>
 					</li>
-					<li><a href="contact.php">Contact</a></li>
-					<li><a class="btn" href="signin.php">SIGN IN / SIGN UP</a></li>
+					<li><a href="contact.php">Contacto</a></li>
+					<li><a class="btn" href="signin.php">INICIAR SESIÓN / REGISTRARSE</a></li>
 				</ul>
 			</div><!--/.nav-collapse -->
 		</div>
@@ -57,9 +57,9 @@
 	<header id="head">
 		<div class="container">
 			<div class="row">
-				<h1 class="lead">AWESOME, CUSTOMIZABLE, FREE</h1>
-				<p class="tagline">PROGRESSUS: free business bootstrap template by <a href="http://www.gettemplate.com/?utm_source=progressus&amp;utm_medium=template&amp;utm_campaign=progressus">GetTemplate</a></p>
-				<p><a class="btn btn-default btn-lg" role="button">MORE INFO</a> <a class="btn btn-action btn-lg" role="button">DOWNLOAD NOW</a></p>
+				<h1 class="lead">IMPRESIONANTE, PERSONALIZABLE, GRATIS</h1>
+				<p class="tagline">PROGRESSUS: plantilla de arranque empresarial gratuita de <a href="http://www.gettemplate.com/?utm_source=progressus&amp;utm_medium=template&amp;utm_campaign=progressus">GetTemplate</a></p>
+				<p><a class="btn btn-default btn-lg" role="button">MÁS INFORMACIÓN</a> <a class="btn btn-action btn-lg" role="button">DESCARGAR AHORA</a></p>
 			</div>
 		</div>
 	</header>
